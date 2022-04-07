55e46af8db3e85e58dcaa6d109a7886d59f8e1e8
Nikita535 <68530909+Nikita535@users.noreply.github.com>
2022-04-07 22:04:43 +0300

    Initial commit

---
+++ src/main/java/demo/Entity/User.java
@@ -0,0 +1,111 @@
+//package demo.Entity;
+//
+//
+//import org.springframework.security.core.GrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//
+//import javax.persistence.*;
+//import javax.validation.constraints.Size;
+//import java.util.Collection;
+//
+//@Entity
+//@Table(name = "users")
+//public class User implements UserDetails {
+//
+//
+//    @Id
+//    @GeneratedValue(strategy=GenerationType.SEQUENCE)
+//    private int id;
+//
+//    @Size(min=2, message = "Не меньше 5 знаков")
+//    private String username;
+//    @Size(min=2, message = "Не меньше 5 знаков")
+//    private String password;
+//    @Transient
+//    private String passwordConfirm;
+//    private String email;
+//
+//    public User() {
+//    }
+//
+//    public User(String username, String password, String passwordConfirm, String email) {
+//        this.username = username;
+//        this.password = password;
+//        this.passwordConfirm = passwordConfirm;
+//        this.email = email;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return null;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public String getPasswordConfirm() {
+//        return passwordConfirm;
+//    }
+//
+//    public void setPasswordConfirm(String passwordConfirm) {
+//        this.passwordConfirm = passwordConfirm;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "\nUser{" +
+//                "id=" + id +
+//                ", username='" + username + '\'' +
+//                ", password='" + password + '\'' +
+//                '}';
+//    }
+//}

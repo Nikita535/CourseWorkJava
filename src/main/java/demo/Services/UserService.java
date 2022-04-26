6302da38081444a3ec77938dde9490b43a2948fa
Nikita535 <68530909+Nikita535@users.noreply.github.com>
2022-04-26 21:52:14 +0300

    ad verification

--- src/main/java/demo/Services/UserService.java
+++ src/main/java/demo/Services/UserService.java
@@ -84,7 +84,7 @@ public class UserService implements UserDetailsService {
 
         if (!StringUtils.isEmpty(user.getEmail())) {
             String message = String.format(
-                    "Hello, %s! \n" + "Welcome to Sweater. Please, visit next link: http://localhost:8080/activate/%s",
+                    "Привет, %s! \n" + "Добро пожаловать. Пожалуйста, перейдите по ссылке: http://localhost:8080/activate/%s",
                     user.getUsername(),
                     user.getActivationCode()
             );
